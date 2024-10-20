e364daebc3d91bfe02b10d855eb25859333e4104
Sai Phanichandra <saiphanichandrachittaluri@gmail.com>
2024-10-21 01:27:05 +0530

    update2

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('gnana_gamya'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro','robot_core.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
